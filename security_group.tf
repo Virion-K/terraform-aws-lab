6e3414aae9f7122c16bab4a4505db0e207fdbe4f
KC Booe <92347797+Virion-K@users.noreply.github.com>
2026-05-31 09:57:28 -0400

    Add VPC and security group lab

---
+++ security_group.tf
@@ -0,0 +1,32 @@
+resource "aws_vpc" "lab" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "terraform-lab-vpc"
+  }
+}
+
+resource "aws_security_group" "rdp_lab" {
+  name        = "terraform-rdp-lab"
+  description = "RDP access lab"
+  vpc_id      = aws_vpc.lab.id
+
+  ingress {
+    description = "RDP"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-rdp-lab"
+  }
+}
