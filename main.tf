89ca7b54857c53e66bd0c396c361ea3fbc52e805
KC Booe <92347797+Virion-K@users.noreply.github.com>
2026-05-31 08:48:44 -0400

    Add variables and outputs

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "lab" {
-  bucket = "virion-k-terraform-lab-2026-001"
+  bucket = var.bucket_name
+
+  tags = {
+    Project     = "Terraform AWS Lab"
+    Environment = "Lab"
+    Owner       = "Virion-K"
+  }
 }
